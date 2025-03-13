d12e739f59fc70b7c3e986db3e50143232370259
PrabhakarCK <kpabbya@gmail.com>
2025-03-13 17:17:36 -0400

    Print Roman number

---
+++ src/PrintRomanNumber.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class PrintRomanNumber {
+
+    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+    static {
+
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+    }
+
+    public final static String toRoman(int number) {
+        int l = map.floorKey(number);
+        if (number == l) {
+            return map.get(number);
+        }
+        return map.get(l) + toRoman(number - l);
+    }
+
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println(number + "->" + "\"" + PrintRomanNumber.toRoman(number) + "\"");
+    }
+
+}
